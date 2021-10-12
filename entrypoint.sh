6e75db6fcbf2adcaef8e9171c3787fe8bc4570d9
Jeroen Rietveld <anywherepilot@github.com>
2021-10-12 17:04:10 +0900

    Add initial installation steps

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,19 @@
 #!/bin/sh -l
 
-echo "Hello"
+echo "Updating apt"
+apt update
+
+echo "Installing Ruby"
+apt install ruby-full -y
+
+echo "Installing Bundler"
+gem install bundler
+
+# TODO create Gemfile
+
+echo "Running Bundler"
+bundle install
+
 FASTFILE=fastlane/AppFile
 if [[ -f "$FASTFILE" ]]; then
     echo "fastlane file exists."
